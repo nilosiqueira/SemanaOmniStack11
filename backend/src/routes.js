dfdaee73e97210187cd66b17e7dda84f7ba4b17f
Nilo <nilosiqueiratj@gmail.com>
2020-04-11 22:52:18 -0300

    orm knex configuracao

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,22 @@
 import { Router } from 'express';
+import OngController from './app/controllers/OngController';
+import IncidentController from './app/controllers/IncidentController';
+import ProfileController from './app/controllers/ProfileController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: "Hello Worldy"})
-})
+routes.post('/sessions', SessionController.store);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.store);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.store);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+
 
 export default routes;
